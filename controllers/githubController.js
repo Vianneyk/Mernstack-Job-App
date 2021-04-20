c35ea3fb52b20a8e27c79beef54f80dbf6fb1681
Vianney Kabamba <vianneyk@hotmail.co.uk>
2021-04-20 18:18:07 +0100

    controller file created

---
+++ controllers/githubController.js
@@ -0,0 +1,18 @@
+const axios = require("axios");
+
+module.exports = {
+	search: function (req, res) {
+		const { search } = req.query;
+
+		axios
+			.get("https://jobs.github.com/positions.json", {
+				params: search,
+			})
+			.then(response => {
+				console.log("response: ", response);
+
+				res.json(response.data);
+			})
+			.catch(err => res.status(422).json(err));
+	},
+};
