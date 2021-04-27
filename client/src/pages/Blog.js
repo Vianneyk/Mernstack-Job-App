dbffdf3198c4cce34729e7a05d761ef78b5444ef
Chioma Onyeguke <chiomaonyeguke1@gmail.com>
2021-04-27 21:52:17 +0100

    changes

--- client/src/pages/Blog.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Blog = () => {
-	return <h1>Hello I'm the Blog page</h1>;
-};
-
-export default Blog;
--- client/src/pages/Home.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Home = () => {
-	return <h1>Hello I'm the home page</h1>;
-};
-
-export default Home;
