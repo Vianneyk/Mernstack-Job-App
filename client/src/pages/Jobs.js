c03258d861899bb917190e0b7805fc8e98da2087
Vianney Kabamba <vianneyk@hotmail.co.uk>
2021-04-27 20:18:07 +0100

    API parameters modified and styling added to jobs card

--- client/src/pages/Jobs.js
+++ client/src/pages/Jobs.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useHistory } from "react-router-dom";
+import { Card, Badge, Button, Collapse } from "react-bootstrap";
 
 import API from "../utils/API";
 
@@ -8,24 +9,13 @@ const { findJobs } = API;
 const Job = () => {
 	const [search, setSearch] = useState("");
 	const [jobs, setJobs] = useState([]);
-
-	// useEffect(() => {
-	// 	loadJobs()
-	//   }, [])
-	
-	//   function loadJobs() {
-	// 	API.findJobs()
-	// 	  .then(res => 
-	// 		setJobs(res.data)
-	// 	  )
-	// 	  .catch(err => console.log(err));
-	//   };
+	const [open, setOpen] = useState(false);
 
 	const handleSubmit = e => {
 		e.preventDefault();
 
 		findJobs(search)
-			.then(({data}) => setJobs(data))
+			.then(({ data }) => setJobs(data))
 			.catch(err => console.log(err));
 	};
 
@@ -33,15 +23,15 @@ const Job = () => {
 		setSearch(e.target.value);
 	};
 
-	const handleApplyClick = (link) =>{
-		window.open(link, "_blank")
+	const handleApplyClick = link => {
+		window.open(link, "_blank");
 	};
 	//not functional
-	const handleSaveClick = (link) => {
-		window.open(link, "_blank")
-	}
+	const handleSaveClick = link => {
+		window.open(link, "_blank");
+	};
 	return (
-		<div>
+		<Card>
 			<div>
 				<form onSubmit={handleSubmit}>
 					<input value={search} onChange={handleChange} />
@@ -51,20 +41,57 @@ const Job = () => {
 			<div>
 				<ul>
 					{jobs.map((job, index) => {
-						return <div> <li key={index}>{job.title}
-						<a href = {job.company_url}>
-						<img src = {job.company_logo} width= "200px" alt ="company logo"/>
-						</a>
-						<p>{job.location}</p>
-						<p>{job.description}</p>
-						<button onClick ={() => handleApplyClick(job.how_to_apply)}>Apply</button>
-						<button onClick ={() => handleSaveClick()}>Save</button>
-						</li>
-						</div>;
+						return (
+							<Card className="mb-3">
+								<Card.Body>
+									<div className="d-flex justify-content-between">
+										<div>
+											<Card.Title>
+												{job.title} -{" "}
+												<span className="text-muted font-weight-light">
+													{job.company}
+												</span>
+											</Card.Title>
+											<Card.Subtitle className="text-muted mb-2">
+												{new Date(job.created_at).toLocaleDateString()}
+											</Card.Subtitle>
+											<Badge variant="secondary" className="mr-2">
+												{job.type}
+											</Badge>
+											<Badge variant="secondary">{job.location}</Badge>
+											<div
+												style={{ wordBreak: "break-all" }}
+												dangerouslySetInnerHTML={{ __html: job.how_to_apply }}
+											></div>
+											<Card.Text>
+												<Button
+													onClick={() => setOpen(prevOpen => !prevOpen)}
+													variant="primary"
+												>
+													{open ? "Hide Details" : "View Details"}
+												</Button>
+											</Card.Text>
+											<Collapse in={open}>
+												<div
+													className="mt-4"
+													dangerouslySetInnerHTML={{ __html: job.description }}
+												></div>
+											</Collapse>
+										</div>
+										<img
+											className="d-none d-md-block"
+											height="50"
+											alt={job.company}
+											src={job.company_logo}
+										/>
+									</div>
+								</Card.Body>
+							</Card>
+						);
 					})}
 				</ul>
 			</div>
-		</div>
+		</Card>
 	);
 };
 
--- controllers/githubController.js
+++ controllers/githubController.js
@@ -1,19 +1,23 @@
 const axios = require("axios");
 
 module.exports = {
-	search: function (req, res) {
-		const { search } = req.query;
+	search: async (req, res) => {
+		try {
+			let { description = "", full_time, location = "", page = 1 } = req.query;
 
-		axios
-			.get("https://jobs.github.com/positions.json?markdown=true&location=europe", {
-				params: search,
-			})
-			.then(response => {
-				console.log("response: ", response);
-
-				res.json(response.data);
-			})
-			.catch(err => res.status(422).json(err));
+			description = description ? encodeURIComponent(description) : "";
+			location = location ? encodeURIComponent(location) : "";
+			full_time = full_time === "true" ? "&full_time=true" : "";
+			if (page) {
+				page = parseInt(page);
+				page = isNaN(page) ? "" : `&page=${page}`;
+			}
+			const query = `https://jobs.github.com/positions.json?description=${description}&location=${location}${full_time}${page}`;
+			const result = await axios.get(query);
+			res.send(result.data);
+		} catch (error) {
+			res.status(400).send("Error while getting list of jobs.Try again later.");
+		}
 	},
 };
 
